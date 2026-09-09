49da3443db14bf37c4ce77ec5df43bbeaef813f3
pm2979 <pm29798512@gmail.com>
2026-09-09 19:46:26 +0900

    feat: 게임 생성 요청/응답 DTO를 API 명세에 맞게 구현 (Lv5)
    RunCardRequest에 카드 필드 제약 조건을 Bean Validation으로 추가하고,
    CardResponse에 명세의 카드 응답 필드를 채움

--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,19 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
     // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    @NotBlank
     private String cardType;
+
+    @NotNull
+    @Min(0)
+    @Max(10)
     private Integer acquiredFloor;
 }
